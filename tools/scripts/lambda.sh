bc2bdae950046ac6cf2543e78843b0319632234f
Chris Kennedy <cmkennedy20@gmail.com>
2023-10-20 21:24:41 -0400

    Remove extra space

--- tools/scripts/lambda.sh
+++ tools/scripts/lambda.sh
@@ -6,6 +6,5 @@ for folder in "$working_directory"/*; do
         lambda_name=$(basename $folder)
         zip "$lambda_name.zip" "$working_directory/$lambda_name/"
         # awslocal lambda create-function --function-name $lambda_name --runtime nodejs18.x --zip-file "fileb://$lambda_name.zip" --handler index.handler --role arn:aws:iam::000000000000:role/lambda-role
-
         awslocal lambda create-function-url-config --function-name $lambda_name --auth-type NONE
     fi
