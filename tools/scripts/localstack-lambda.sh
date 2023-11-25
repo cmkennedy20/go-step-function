5abf7ed5234f976a4747bc5df35f70780aadc604
cmkennedy20 <cmkennedy20@gmail.com>
2023-11-25 10:33:01 -0500

    Relocate the AWS_PAGER environement variable

--- tools/scripts/localstack-lambda.sh
+++ tools/scripts/localstack-lambda.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /opt/lambda_home
+export AWS_PAGER=""
 working_directory=$(pwd)
 for folder in "$working_directory"/*; do
     if [ -d "$folder" ]; then
@@ -8,9 +9,6 @@ for folder in "$working_directory"/*; do
         awslocal lambda create-function --function-name $lambda_name --runtime nodejs18.x --zip-file "fileb://$lambda_name.zip" --handler index.handler --role arn:aws:iam::000000000000:role/lambda-role
         awslocal lambda create-function-url-config --function-name $lambda_name --auth-type NONE
     fi
-
-
-export AWS_PAGER=""
 cd /opt/localstack_home
 work_dir=$(pwd)
 functions=$(ls | grep lambda)
